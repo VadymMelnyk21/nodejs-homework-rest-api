99fbee84076d42ac6cd0f65fe0f3cb6b23f12259
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-08-28 20:52:30 +0300

    part4_step1

---
+++ models/user.js
@@ -0,0 +1,52 @@
+const { Schema, model } = require('mongoose');
+const Joi = require('joi');
+
+const userSchema = Schema(
+    {
+        password: {
+            type: String,
+            required: [true, 'Set password for user'],
+        },
+        email: {
+            type: String,
+            required: [true, 'Email is required'],
+            unique: true,
+        },
+        subscription: {
+            type: String,
+            enum: ["starter", "pro", "business"],
+            default: "starter"
+        },
+        token: {
+            type: String,
+            default: null,
+        },
+    },
+    { versionKey: false, timestamps: true });
+
+const registerSchema = Joi.object({
+    password: Joi.string().trim().required(),
+    email: Joi.string().trim().required(),
+    subscription: Joi.string()
+        .default('starter')
+        .valid('starter', 'pro', 'business'),
+    token: Joi.string().required(),
+});
+
+const loginSchema = Joi.object({
+    password: Joi.string().trim().required(),
+    email: Joi.string().trim().required(),
+});
+
+const subscriptionSchema = Joi.object({
+    subscription: Joi.any().valid('starter', 'pro', 'business'),
+})
+
+const User = model('user', userSchema);
+
+module.exports = {
+    User,
+    registerSchema,
+    loginSchema,
+    subscriptionSchema,
+}
