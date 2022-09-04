df07f71b56e622c2aba6a93badc38089cf68914e
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-09-04 15:45:14 +0300

    task5_step2-3

--- controllers/users/register.js
+++ controllers/users/register.js
@@ -1,6 +1,7 @@
 const { User } = require('../../models/user');
 const { RequestError } = require('../../helpers');
 const bcrypt = require('bcryptjs');
+const gravatar = require('gravatar');
 
 const register = async (req, res) => {
     const { email, password } = req.body;
@@ -11,7 +12,8 @@ const register = async (req, res) => {
 
     const hashPass = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
 
-    const result = await User.create({ email, password: hashPass });
+    const avatarURL = gravatar.url(email);
+    const result = await User.create({ email, password: hashPass, avatarURL });
     res.status(201).json({
         status: 'created',
         code: 201,
